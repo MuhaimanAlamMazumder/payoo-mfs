cf670111f1d8365e2b3a27f8c7fb4933ca7766da
MuhaimanAlamMazumder <muhaimennabil@gmail.com>
2024-10-28 13:31:22 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,10 @@
+ document.getElementById('button-login').addEventListener('click', function(event){
+
+     event.preventDefault();
+
+     console.log('login button clicked');
+
+     const phoneNumber = document.getElementById('phone-number').value;
+     console.log(phoneNumber)
+})
+
